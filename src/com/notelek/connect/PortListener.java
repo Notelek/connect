28e7997ad26e23606211c75e895051ad0d7a9b6c
Matt Foulks <mfoulks1@gmail.com>
2015-10-23 14:58:07 -0400

    Fixed final boolean
    Travis CI picthed a fit :(

--- src/com/notelek/connect/PortListener.java
+++ src/com/notelek/connect/PortListener.java
@@ -11,7 +11,7 @@ public class PortListener
     private static final int fNumberOfThreads = 100;
     private static final Executor fThreadPool = Executors.newFixedThreadPool(fNumberOfThreads);
  
-    public PortListener(int port, final WebInterface intFace, boolean acceptOutsideReq) throws Exception{
+    public PortListener(int port, final WebInterface intFace, final boolean acceptOutsideReq) throws Exception{
         @SuppressWarnings("resource")
 		ServerSocket socket = new ServerSocket(port);
         socket.setReuseAddress(true);
