1e5fc4068c85e95ecd1ac78544cbd3003d159a93
Matt Foulks <mfoulks1@gmail.com>
2015-10-24 10:10:02 -0400

    Added Version Management

--- src/com/notelek/connect/PortListener.java
+++ src/com/notelek/connect/PortListener.java
@@ -12,6 +12,7 @@ public class PortListener
     private static final Executor fThreadPool = Executors.newFixedThreadPool(fNumberOfThreads);
  
     public PortListener(int port, final WebInterface intFace, final boolean acceptOutsideReq) throws Exception{
+    	Connect.printStartupInfo();
         @SuppressWarnings("resource")
 		ServerSocket socket = new ServerSocket(port);
         socket.setReuseAddress(true);
